017c9f952c80153cb9f6d45b0fe9a2bafca8190d
dramenti <empoleon5000@yahoo.com>
2016-02-11 00:31:32 -0800

    Add more to tokenizer

--- Lexer.java
+++ Lexer.java
@@ -5,7 +5,8 @@ public class Lexer {
     private BufferedReader reader;
     private char current_char;
     private State current_state;
-    private String current_token;
+    private StringBuilder current_token;
+    private State[][] table_f; //refactor as EnumMap later
 
     private State transition(Typechar c) {
         return table_f[current_state][c];
@@ -21,6 +22,28 @@ public class Lexer {
         }
         if (current_state == SFINAL_ERROR) {
             //throw `invalid token` error
+        }
+        else if (current_state == SFINAL_LPAREN) {
+            return new Token(LPAREN);
+        }
+        else if (current_state == SFINAL_RPAREN) {
+            return new Token(RPAREN);
+        }
+        else if (current_state == SFINAL_INTEGER) {
+            return new Token(INTEGER, Integer.parseInt(current_token.toString()));
+        }
+        else if (current_state == SFINAL_FLOAT) {
+            return new Token(FLOAT, Double.parseDouble(current_token.toString()));
+        }
+        else if (current_state == SFINAL_STRING) {
+            current_char = reader.read();
+            return new Token(STRING, current_token.toString());
+        }
+        else if (current_state == SFINAL_ID) {
+            //
+        }
+        else {
+            //throw error
         }
     }
 
@@ -50,7 +73,7 @@ public class Lexer {
         SFINAL_INTEGER,
         SFINAL_FLOAT,
         SFINAL_STRING,
-        SFINAL_ERROR
+        SFINAL_ERROR;
     }
     private enum Typechar {
         TC_QUOTE,
@@ -60,54 +83,58 @@ public class Lexer {
         TC_HYPHEN,
         TC_DIGIT,
         TC_IDSTART,
-        TC_OTHER
+        TC_OTHER;
     }
     public Lexer() {
-        //initialize the finite state machine
-        table_f[S_INITIAL][TC_QUOTE] = S_STRING;
-        table_f[S_INITIAL][TC_LPAREN] = SFINAL_LPAREN;
-        table_f[S_INITIAL][TC_RPAREN] = SFINAL_RPAREN;
-        table_f[S_INITIAL][TC_DOT] = S_FLOAT;
-        table_f[S_INITIAL][TC_HYPHEN] = S_NUMERIC;
-        table_f[S_INITIAL][TC_DIGIT] = S_NUMERIC;
-        table_f[S_INITIAL][TC_IDSTART] = S_ID;
-        table_f[S_INITIAL][TC_OTHER] = SFINAL_ERROR;
+        //initialize the table for FSM
+        final int ROWS = 5;
+        final int COLS = Typechar.values().length;
+        table_f = new State[ROWS][COLS];
+        //initialize the FSM itself
+        table_f[S_INITIAL.ordinal()][TC_QUOTE.ordinal()] = S_STRING;
+        table_f[S_INITIAL.ordinal()][TC_LPAREN.ordinal()] = SFINAL_LPAREN;
+        table_f[S_INITIAL.ordinal()][TC_RPAREN.ordinal()] = SFINAL_RPAREN;
+        table_f[S_INITIAL.ordinal()][TC_DOT.ordinal()] = S_FLOAT;
+        table_f[S_INITIAL.ordinal()][TC_HYPHEN.ordinal()] = S_NUMERIC;
+        table_f[S_INITIAL.ordinal()][TC_DIGIT.ordinal()] = S_NUMERIC;
+        table_f[S_INITIAL.ordinal()][TC_IDSTART.ordinal()] = S_ID;
+        table_f[S_INITIAL.ordinal()][TC_OTHER.ordinal()] = SFINAL_ERROR;
 
-        table_f[S_STRING][TC_QUOTE] = SFINAL_STRING;
-        table_f[S_STRING][TC_LPAREN] = S_STRING;
-        table_f[S_STRING][TC_RPAREN] = S_STRING;
-        table_f[S_STRING][TC_DOT] = S_STRING;
-        table_f[S_STRING][TC_HYPHEN] = S_STRING;
-        table_f[S_STRING][TC_DIGIT] = S_STRING;
-        table_f[S_STRING][TC_IDSTART] = S_STRING;
-        table_f[S_STRING][TC_OTHER] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_QUOTE.ordinal()] = SFINAL_STRING;
+        table_f[S_STRING.ordinal()][TC_LPAREN.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_RPAREN.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_DOT.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_HYPHEN.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_DIGIT.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_IDSTART.ordinal()] = S_STRING;
+        table_f[S_STRING.ordinal()][TC_OTHER.ordinal()] = S_STRING;
 
-        table_f[S_NUMERIC][TC_QUOTE] = SFINAL_INTEGER;
-        table_f[S_NUMERIC][TC_LPAREN] = SFINAL_INTEGER;
-        table_f[S_NUMERIC][TC_RPAREN] = SFINAL_INTEGER;
-        table_f[S_NUMERIC][TC_DOT] = S_FLOAT;
-        table_f[S_NUMERIC][TC_HYPHEN] = SFINAL_INTEGER;
-        table_f[S_NUMERIC][TC_DIGIT] = S_NUMERIC;
-        table_f[S_NUMERIC][TC_IDSTART] = SFINAL_INTEGER;
-        table_f[S_NUMERIC][TC_OTHER] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_QUOTE.ordinal()] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_LPAREN.ordinal()] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_RPAREN.ordinal()] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_DOT.ordinal()] = S_FLOAT;
+        table_f[S_NUMERIC.ordinal()][TC_HYPHEN.ordinal()] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_DIGIT.ordinal()] = S_NUMERIC;
+        table_f[S_NUMERIC.ordinal()][TC_IDSTART.ordinal()] = SFINAL_INTEGER;
+        table_f[S_NUMERIC.ordinal()][TC_OTHER.ordinal()] = SFINAL_INTEGER;
 
-        table_f[S_FLOAT][TC_QUOTE] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_LPAREN] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_RPAREN] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_DOT] = S_FLOAT;
-        table_f[S_FLOAT][TC_HYPHEN] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_DIGIT] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_IDSTART] = SFINAL_FLOAT;
-        table_f[S_FLOAT][TC_OTHER] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_QUOTE.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_LPAREN.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_RPAREN.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_DOT.ordinal()] = S_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_HYPHEN.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_DIGIT.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_IDSTART.ordinal()] = SFINAL_FLOAT;
+        table_f[S_FLOAT.ordinal()][TC_OTHER.ordinal()] = SFINAL_FLOAT;
 
-        table_f[S_ID][TC_QUOTE] = SFINAL_ID;
-        table_f[S_ID][TC_LPAREN] = SFINAL_ID;
-        table_f[S_ID][TC_RPAREN] = SFINAL_ID;
-        table_f[S_ID][TC_DOT] = S_ID;
-        table_f[S_ID][TC_HYPHEN] = SFINAL_ID;
-        table_f[S_ID][TC_DIGIT] = S_ID;
-        table_f[S_ID][TC_IDSTART] = S_ID;
-        table_f[S_ID][TC_OTHER] = SFINAL_ID;
+        table_f[S_ID.ordinal()][TC_QUOTE.ordinal()] = SFINAL_ID;
+        table_f[S_ID.ordinal()][TC_LPAREN.ordinal()] = SFINAL_ID;
+        table_f[S_ID.ordinal()][TC_RPAREN.ordinal()] = SFINAL_ID;
+        table_f[S_ID.ordinal()][TC_DOT.ordinal()] = S_ID;
+        table_f[S_ID.ordinal()][TC_HYPHEN.ordinal()] = SFINAL_ID;
+        table_f[S_ID.ordinal()][TC_DIGIT.ordinal()] = S_ID;
+        table_f[S_ID.ordinal()][TC_IDSTART.ordinal()] = S_ID;
+        table_f[S_ID.ordinal()][TC_OTHER.ordinal()] = SFINAL_ID;
     }
     public Token next_token() {
         current_state = S_INITIAL;
