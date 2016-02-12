0b762cfc488c5441ed1cebdc3bffa9d8d94a2185
dramenti <empoleon5000@yahoo.com>
2016-02-12 14:38:21 -0800

    Implement visitor pattern

--- jayanth/junction/AST_Evaluator.java
+++ jayanth/junction/AST_Evaluator.java
@@ -2,4 +2,31 @@ package jayanth.junction;
 
 public class AST_Evaluator implements AST_Visitor {
 
+    JunObject visit(CallNode node, JunFrame frame) {
+        //evaluate the caller expression
+        Function function = node.caller().accept(this);
+        //get # of args for function
+        //for each arg of the function, get the JunObject of the node's
+        //next one
+        JunFrame child = new JunFrame(frame); //parent=frame
+        for (int i = 0; i < function.getArgCount(); i++) {
+            AST_Node arg = node.getIthArgumentNode(i);
+            JunObject parameter = arg.accept(this);
+            child.add(new String(function.getIthFormal(i)), parameter);
+        }
+    }
+    JunObject visit(DefNode node, JunFrame frame) {
+        Function function = new Function(node.getFunctionName(), node.getFormals(), frame);
+        frame.add(new String(node.getFunctionName()), function);
+        return function;
+    }
+    JunObject visit(LambdaNode node, JunFrame frame) {
+    }
+    JunObject visit(BooleanNode node, JunFrame frame);
+    JunObject visit(IdentifierNode node, JunFrame frame) {
+        return frame.getValueInCurrentEnvironment(node.getName());
+    }
+    JunObject visit(StringNode node, JunFrame frame);
+    JunObject visit(FloatNode node, JunFrame frame);
+    JunObject visit(IntNode node, JunFrame frame);
 }
--- jayanth/junction/JunFrame.java
+++ jayanth/junction/JunFrame.java
@@ -11,4 +11,22 @@ public class JunFrame {
     public Map<String, JunObject> getSymbolTable() {
         return symbol_table;
     }
+    public JunFrame getParent() {
+        return parent;
+    }
+
+    public void add(String name, JunObject value) {
+        symbol_table.put(name, value);
+    }
+    public JunObject getValueInCurrentEnvironment(String name) {
+        JunObject value;
+        JunFrame frame = this;
+        while (frame != null) {
+            value = frame.getSymbolTable.get(name);
+            if (value != null) return value;
+            frame = frame.getParent();
+        }
+        //value was always null, Name Error!
+        return null; //ERROR! Name `name` not defined in current environment
+    }
 }
